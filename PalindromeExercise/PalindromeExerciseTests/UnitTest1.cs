e7f1e74b3ef167603b9773e2f0c2e759470afa34
Kevin March <marchkevin78@gmail.com>
2020-10-23 19:36:58 -0400

    CompletedPalindromeExercise

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,25 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("Racecar", true)]
+        public void MyTest(string word, bool expected)
         {
+            //arrange
+            var tester = new WordSmith();
 
+            //act
+            var actual = tester.IsAPalindrome(word);
+
+            //assert
+            Assert.Equal(expected, actual);
         }
     }
 }
